9ac4216a0b76db856eef411fdd276b4a32d88b70
ppondo <patrickpondo7@gmail.com>
2019-07-01 16:31:31 -0400

    reorganize javascript files

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,139 +39,138 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  var tech = document.getElementById('tech')
-  tech.onchange = function() {
-    tech.style.transition='1000ms ease-out'
-  }
-  var react = document.getElementById('react')
-  var redux = document.getElementById('redux')
-  var javascript = document.getElementById('javascript')
-  var ruby = document.getElementById('ruby')
-  var rails = document.getElementById('rails')
-  var post = document.getElementById('post')
-  var html = document.getElementById('html')
-  var css = document.getElementById('css')
-
-  //React
-  react.onmouseover = function() {
-    tech.style.opacity = 0;
-    setTimeout(function() {
-      tech.innerHTML = 'React.js';
-      tech.style.opacity = 1;
-    }, 400); 
-  }
-  react.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function() {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //Redux
-  redux.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {    
-      tech.innerHTML = 'Redux.js';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  redux.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //JavaScript
-  javascript.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'JavaScript';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  javascript.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //Ruby
-  ruby.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'Ruby';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  ruby.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //Rails
-  rails.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'Ruby on Rails';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  rails.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //Postgresql
-  post.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'PostgreSQL';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  post.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function() {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //HTML
-  html.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'HTML5';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  html.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function() {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  //CSS
-  css.onmouseover = function () {
-    tech.style.opacity = 0;
-    setTimeout(function () {
-      tech.innerHTML = 'CSS3';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-  css.onmouseleave = function () {
-    tech.style.opacity = 0;
-    setTimeout(function() {
-      tech.innerHTML = 'Developer Toolkit';
-      tech.style.opacity = 1;
-    }, 400);
-  }
-
-
 })(jQuery); // End of use strict
+
+var tech = document.getElementById('tech')
+tech.onchange = function () {
+  tech.style.transition = '1000ms ease-out'
+}
+var react = document.getElementById('react')
+var redux = document.getElementById('redux')
+var javascript = document.getElementById('javascript')
+var ruby = document.getElementById('ruby')
+var rails = document.getElementById('rails')
+var post = document.getElementById('post')
+var html = document.getElementById('html')
+var css = document.getElementById('css')
+
+//React
+react.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'React.js';
+    tech.style.opacity = 1;
+  }, 400);
+}
+react.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//Redux
+redux.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Redux.js';
+    tech.style.opacity = 1;
+  }, 400);
+}
+redux.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//JavaScript
+javascript.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'JavaScript';
+    tech.style.opacity = 1;
+  }, 400);
+}
+javascript.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//Ruby
+ruby.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Ruby';
+    tech.style.opacity = 1;
+  }, 400);
+}
+ruby.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//Rails
+rails.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Ruby on Rails';
+    tech.style.opacity = 1;
+  }, 400);
+}
+rails.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//Postgresql
+post.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'PostgreSQL';
+    tech.style.opacity = 1;
+  }, 400);
+}
+post.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//HTML
+html.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'HTML5';
+    tech.style.opacity = 1;
+  }, 400);
+}
+html.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
+//CSS
+css.onmouseover = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'CSS3';
+    tech.style.opacity = 1;
+  }, 400);
+}
+css.onmouseleave = function () {
+  tech.style.opacity = 0;
+  setTimeout(function () {
+    tech.innerHTML = 'Developer Toolkit';
+    tech.style.opacity = 1;
+  }, 400);
+}
